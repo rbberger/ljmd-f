bf66f26e04f3cfa074ffbe20e0c81757cda9f757
ghagoss <garugeb@gmail.com>
2013-03-19 17:57:33 +0100

    Added output module

---
+++ src/output.py
@@ -0,0 +1,31 @@
+class Output:
+    def __init__(self, parameters):
+        self.logfile=open(parameters.energiesFilename, "w")
+        self.xyzfile=open(parameters.trajectoryFilename, "w")
+        self.parameters = parameters
+
+    def write_header(self):
+        print "Starting Simulation with", self.parameters.natoms, "atoms for", self.parameters.numsteps, "steps"
+        print "NFI\t TMP\t EKIN\t EPOT\t ETOT"
+
+    def output(self, nfi, temp, ekin, epot, positions):
+        self.logfile.write("%d\t%f\t%f\t%f\t%f\n" %  (nfi, temp, ekin, epot, ekin+epot))
+        print "%d\t%f\t%f\t%f\t%f" % (nfi, temp, ekin, epot, ekin+epot)
+        self.xyzfile.write("%d\n" % self.parameters.natoms)
+        self.xyzfile.write("%s%d\t%s%f\n" % ("nfi=", nfi, "etot=", ekin+epot))
+        for p in positions:
+            self.xyzfile.write("(Ar, %f, %f, %f)\n" % p)
+        
+    def close(self):
+        self.logfile.close()
+        self.xyzfile.close()
+
+if __name__ == "__main__":
+    import input as p
+    para =p.Parameters()
+    para.load_from_file("../examples/argon_108.inp")
+    print para.trajectoryFilename
+    out = Output(para)
+    out.write_header()
+    out.output(1,1.0,2.0,3.0,[(1.0, 2.0, 3.0), (4.0, 5.0, 6.0)])
+    out.close()
