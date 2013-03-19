ac272cf86178856bd0f74e29e755256866f35d75
Jorge Troconis <jorge.troconis@gmail.com>
2013-03-19 18:56:19 +0100

    Added IO to main

--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
     p = input.Parameters()			# To load the Parameters
     p.load_from_file("../examples/argon_108.inp")  # To load initial pos and vel
     pd = Pdata.ParticleData("../examples/argon_108.rest", 108) 	
-    out = output.Output()
-    print 'Starting simulation ...'
+    out = output.Output(p)
+
+    out.write_header()
+    print '\nStarting simulation ...'
 
     print "\tnatoms", p.natoms
     print "\tmass", p.mass
@@ -22,13 +24,22 @@ if __name__ == "__main__":
     print "\ttrajectoryFilename", p.trajectoryFilename
     print "\tenergiesFilename", p.energiesFilename  
     pd.write_out("output.rest")
+        
+    #interface.updcell()
+    #interface
+    
+    temp = 2.3
+    ekin = 4.6
+    epot = 5.7
+    pos = [(1.0, 2.0, 3.0), (4.0, 5.0, 6.0)]
     for n in range(int(p.numsteps)):		
-        if n % p.outputfreq:
-             pass
+        if n % p.outputfreq == 0 :
+            out.output(n,temp,ekin,epot,pos)
+            #print n
 
         #interface.updcell()
         #interface
-        print n
+    out.close()
 
 
     print 'End simulation. Bye Bye'
